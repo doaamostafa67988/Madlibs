991d4d24996713b783c583bbf7258da96ada6fcf
doaa mostafa <50427984+doaamostafa67988@users.noreply.github.com>
2022-07-04 13:42:19 +0200

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,11 @@
+#Madlibs using string concatenation
+
+name = input("Name: ")
+prog_lang = input("Programing Language: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"I am  {name}. I always code in {prog_lang} programing language. \
+it makes me so excited all the time . \
+Don't forget to enjoy learning  and having fun like your role model {famous_person}!"
+
